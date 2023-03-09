dbbeee9bbe5b05e269b52adb2710c4620fdb745a
MontBru <80487370+MontBru@users.noreply.github.com>
2023-03-09 15:49:22 +0200

    Update map.dart

--- map.dart
+++ map.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
+
+//for it to work you have to add google_map_flutter to
+//pubspec.yaml and change minimum sdk to 20 in build.gradle
+//and add <meta-data android:name="com.google.android.geo.API_KEY"
+ //          android:value="AIzaSyDlDIqYXzgAuZE5U7c1JxAO7_LHzBbrdoM"/>
+//in androidmanifest             
 class Map extends StatefulWidget {
   const Map({Key? key}) : super(key: key);
 
