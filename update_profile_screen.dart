129315cf05855d797d88329348fa2ba74de0dda7
ItsRizee <101361202+ItsRizee@users.noreply.github.com>
2023-03-09 10:03:38 +0200

    Create update_profile_screen.dart

---
+++ update_profile_screen.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+
+class UpdatedProfileScreen extends StatelessWidget {
+  const UpdatedProfileScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          "Edit Profile",
+          style: Theme.of(context).textTheme.headline6,
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Stack(
+                children: [
+                  SizedBox(
+                    width: 120,
+                    height: 120,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(100),
+                      child: Image.network(
+                          'https://upload.wikimedia.org/wikipedia/commons/e/e9/Felis_silvestris_silvestris_small_gradual_decrease_of_quality.png'),
+                    ),
+                  ),
+                  Positioned(
+                    bottom: 0,
+                    right: 0,
+                    child: Container(
+                      width: 35,
+                      height: 35,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(100),
+                        color: Colors.amber.withOpacity(0.1),
+                      ),
+                      child: const Icon(Icons.camera_alt_outlined,
+                          size: 20, color: Colors.black),
+                    ),
+                  )
+                ],
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              Form(
+                child: Column(
+                  children: [
+                    TextFormField(
+                      decoration: const InputDecoration(label: Text("Full name"),prefixIcon: Icon(Icons.person),),
+                    ),
+                    const SizedBox(height: 30,),
+                    TextFormField(
+                      decoration: const InputDecoration(label: Text("Email"),prefixIcon: Icon(Icons.email),),
+                    ),
+                    const SizedBox(height: 30,),
+                    TextFormField(
+                      decoration: const InputDecoration(label: Text("Password"),prefixIcon: Icon(Icons.lock_open_outlined),),
+                    ),
+                  ]
+                ),
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SizedBox(
+                width: 200,
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.grey,
+                      side: BorderSide.none,
+                      shape: const StadiumBorder()),
+                  child: const Text(
+                    'Cancel',
+                    style: TextStyle(color: Colors.black),
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: 200,
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.amber,
+                      side: BorderSide.none,
+                      shape: const StadiumBorder()),
+                  child: const Text(
+                    'Save',
+                    style: TextStyle(color: Colors.black),
+                  ),
+                ),
+              ),
+              ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
