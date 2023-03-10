8c737b6fe2e1bf7f0e73aa7de5aafc3be350e463
Atefan <127405883+Atefan@users.noreply.github.com>
2023-03-10 23:39:49 +0200

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,32 +1,101 @@
-import 'package:firebase_messaging/firebase_messaging.dart';
-import 'package:project/WIdgetTree.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-
-Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async
-{
-  print('Handling a background message ${message.messageId}');
-}
-
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  await FirebaseMessaging.instance.getInitialMessage();
-  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+import 'package:awesome_notifications/awesome_notifications.dart';
+void main() {
+  AwesomeNotifications().initialize(
+    null,
+    [
+      NotificationChannel(
+          channelKey: 'basic_chanel',
+          channelName: 'Basic notifications',
+          channelDescription: 'Test'
+      )
+    ],
+    debug: true,
+  );
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp ({Key? key}) : super(key: key);
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
 
   @override
-  Widget build(BuildContext context){
-    return MaterialApp(
-      title: 'demo',
-      theme:ThemeData(
-        primarySwatch: Colors.deepOrange,
-      ) ,
-      home: const MainPage(),
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  void initState(){
+    AwesomeNotifications().isNotificationAllowed().then((isAllowed){
+      if(! isAllowed){
+        AwesomeNotifications().requestPermissionToSendNotifications();
+      }
+    });
+    super.initState();
+  }
+
+  triggerNotification()
+  {
+    AwesomeNotifications().createNotification(
+      content: NotificationContent(
+          id: 10,
+          channelKey: 'basic_chanel',
+          title: 'QWERTYUIOUYTREWERTY',
+          body: '123123123'
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: triggerNotification,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
     );
   }
 }
